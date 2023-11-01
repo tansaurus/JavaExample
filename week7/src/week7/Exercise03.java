220f4abb2e234f28e9064b09ba37c3c8fc24ac5a
pacif <pacif@thoro-o505>
2023-11-01 16:43:33 +0900

    fixed

--- week7/src/week7/Exercise03.java
+++ week7/src/week7/Exercise03.java
@@ -4,114 +4,136 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import week7.Radio.MyItemListener;
 
 import java.awt.*;
 import java.awt.event.*;
 
 public class Exercise03 extends JFrame {
 	private JRadioButton[] radio = new JRadioButton[2];
-	private String[] text = {"색상", "크기"};
-	private JLabel[] color = new JLabel[3];
-	private JSlider[] sl = new JSlider[3];
 	private JLabel colorLabel = new JLabel("I Love Coffee");
 	
+	private JLabel[] color = new JLabel[3];
+	private JSlider[] sl = new JSlider[3];
+	
+	private menuPanel MyPanel = new menuPanel();
+	
+	
 	public Exercise03() {
-		
-		setTitle("Exercise03");
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
+		setTitle("EXERCISE_07");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
 		Container c = getContentPane();
-		c.setLayout(new FlowLayout(FlowLayout.CENTER));
+		c.setLayout(null);
 		
-		color[0] = new JLabel("R");
-		color[1] = new JLabel("G");
-		color[2] = new JLabel("B");
+		MyPanel.setBounds(0,0,500,300);
+		colorLabel.setBounds(50,200,500,300);
+		colorLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		c.add(MyPanel);
+		c.add(colorLabel);
 		
-		JPanel radioPanel = new JPanel();
-		ButtonGroup group = new ButtonGroup();
-		for(int i = 0; i < radio.length; i++) {
-		    radio[i] = new JRadioButton();
-		    radio[i].setText(text[i]);
-		    radioPanel.add(radio[i]);
-		    group.add(radio[i]);
-		}
-		radio[0].setSelected(true);
-		c.add(radioPanel, BorderLayout.NORTH);
-		
-		JPanel colorPanel = new JPanel();
-		colorPanel.setLayout(new BoxLayout(colorPanel, BoxLayout.Y_AXIS));
-		for(int i = 0; i < sl.length; i++) {
-			sl[i] = new JSlider(JSlider.HORIZONTAL, 0, 255, 128);
-			sl[i].setPaintLabels(true);
-			sl[i].setPaintTicks(true);
-			sl[i].setPaintTrack(true);
-			sl[i].setMajorTickSpacing(50);
-			sl[i].setMinorTickSpacing(10);
-			sl[i].addChangeListener(new ChangeListener() {
-				public void stateChanged(ChangeEvent e) {
-					int r = sl[0].getValue();
-					int g = sl[1].getValue();
-					int b = sl[2].getValue();
-					colorLabel.setForeground(new Color(r,g,b));
-				}
-			});
-			
-			colorPanel.add(color[i]);
-			colorPanel.add(sl[i]);
-			
-		}
-		
-		c.add(colorPanel);
-		c.add(colorLabel);	
-		
-		
-		
-		radio[1].addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JPanel sizePanel = new JPanel();
-				color[0].setText("크기");
-				color[1].setVisible(false);
-				color[2].setVisible(false);
-					
-				colorLabel.setForeground(Color.BLACK);
-				sl[0].setVisible(false);
-				sl[1].setVisible(false);
-				sl[2].setVisible(false);
-					
-				sl[0] = new JSlider(JSlider.HORIZONTAL, 0, 70, 20);
-				sl[0].setPaintLabels(true);
-				sl[0].setPaintTicks(true);
-				sl[0].setPaintTrack(true);
-				sl[0].setMajorTickSpacing(50);
-				sl[0].setMinorTickSpacing(10);
-				
-				sl[0].addChangeListener(new ChangeListener() {
-					@Override
-					public void stateChanged(ChangeEvent e) {
-						// TODO Auto-generated method stub
-						int size = sl[0].getValue();
-						colorLabel.setFont(new Font("", Font.PLAIN, size));
-					}
-
-				});
-				c.add(sizePanel);
-				sizePanel.add(color[0]);
-				sizePanel.add(sl[0]);
-				c.add(colorLabel);	
-				
-			
-			}
-		});
-
-		colorLabel.setOpaque(true);
-		
-		
-		
-		setSize(300, 400);
+		setSize(500,500);
 		setVisible(true);
 		
-
+	}
+	
+	class menuPanel extends JPanel{
+		public menuPanel() {
+			setLayout(null);
+			
+			ButtonGroup menuGroup = new ButtonGroup();
+			MyItemListener listener = new MyItemListener();
+			
+			radio[0] = new JRadioButton("색상", true);
+			radio[0].addItemListener(listener);
+			radio[1] = new JRadioButton("크기");
+			radio[1].addItemListener(listener);
+			
+			menuGroup.add(radio[0]);
+			menuGroup.add(radio[1]);
+			
+			radio[0].setSize(100,50);
+			radio[0].setLocation(50,20);
+			
+			radio[1].setSize(100,50);
+			radio[1].setLocation(250,20);
+			
+			add(radio[0]);
+			add(radio[1]);
+			
+			for(int i = 0; i < sl.length; i++) {
+				sl[i] = new JSlider(JSlider.HORIZONTAL,0,255,128);
+				sl[i].setPaintLabels(true);
+				sl[i].setPaintTicks(true);
+				sl[i].setPaintTrack(true);
+				sl[i].setMajorTickSpacing(50);
+				sl[i].setMinorTickSpacing(10);
+				sl[i].addChangeListener(new MyChangeListener());
+				sl[i].setSize(300,50);
+				sl[i].setLocation(100, 60*i+90);
+				add(sl[i]);
+			}
+			
+			for(int i = 0; i < color.length; i++) {
+				color[i] = new JLabel();
+				color[i].setSize(50,50);
+				color[i].setLocation(20, 60*i+90);
+				add(color[i]);
+			}
+			color[0].setText("R");
+			color[1].setText("G");
+			color[2].setText("B");
+			
+		}
+	}
+	
+	class MyItemListener implements ItemListener{
+		public void itemStateChanged(ItemEvent e) {
+			if(e.getStateChange() == ItemEvent.DESELECTED)
+				return;
+			
+			if(radio[1].isSelected()) {
+				sl[0].setMaximum(70);
+				sl[0].setValue(35);
+				sl[0].setPaintLabels(true);
+				sl[0].setMajorTickSpacing(10);
+				sl[0].setMinorTickSpacing(5);
+				colorLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+				colorLabel.setForeground(new Color(0,0,0));
+				color[0].setText("크기");
+				
+				for(int i = 1; i < sl.length; i++) {
+					sl[i].setVisible(false);
+					color[i].setVisible(false);
+				}
+			}
+			if(radio[0].isSelected()) {
+				sl[0].setMaximum(255);
+				sl[0].setValue(127);
+				sl[0].setMajorTickSpacing(50);
+				sl[0].setMinorTickSpacing(10);
+				color[0].setText("R");
+				
+				for(int i=1; i < sl.length; i++) {
+					sl[i].setVisible(true);
+					sl[i].setValue(127);
+					color[i].setVisible(true);
+				}
+				colorLabel.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+			}
+		}
+	}
+	
+	class MyChangeListener implements ChangeListener{
+		public void stateChanged(ChangeEvent e) {
+			if(sl[2].isVisible()) {
+				int r = sl[0].getValue();
+				int g = sl[1].getValue();
+				int b = sl[2].getValue();
+				colorLabel.setForeground(new Color(r,g,b));
+			}
+			if(!sl[2].isVisible())
+				colorLabel.setFont(new Font("TimesRoman", Font.PLAIN, sl[0].getValue()));
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
